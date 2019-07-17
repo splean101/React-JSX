20739148bbd8484de4cf8904ea2e608abda60b90
splean101 <101splean@gmail.com>
2019-07-17 16:28:51 +0300

    fin

--- src/index.js
+++ src/index.js
@@ -4,30 +4,19 @@ import ReactDOM from 'react-dom';
 let arr = [[23, 32], [69, 96]];
 
 function createReactTable(data, container) {
-    
     ReactDOM.render(<table id='table'>
-    <tbody>
-        <tr>
-            <td>
-            {data[0][0]}
-            </td>
-            <td>
-            {data[0][1]}
-            </td>
-        </tr>
-        <tr>
-            <td>
-            {data[1][0]}
-            </td>
-            <td>
-            {data[1][1]}
-            </td>
-        </tr>
-    </tbody>
-</table>, 
-document.getElementById(container));
-}
-
-
+        <tbody>
+            <tr>
+                <td>{data[0][0]}</td>
+                <td>{data[0][1]}</td>
+            </tr>
+            <tr>
+                <td>{data[1][0]}</td>
+                <td>{data[1][1]}</td>
+            </tr>
+        </tbody>
+    </table>,
+        document.getElementById(container));
+};
 
 createReactTable(arr, 'div1');
